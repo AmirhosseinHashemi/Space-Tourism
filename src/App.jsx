32deeb6d059ad7449936dd2a9750453f197e73f7
Amirhossein <4mirh03einhashemi@gmail.com>
2024-06-01 20:47:43 +0330

    implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import AppLayout from "./components/AppLayout";
 
 function App() {
-  return <AppLayout></AppLayout>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate to="home" replace />} />
+          <Route path="home" element={<Home />} />
+          <Route path="destination" element={<Destination />} />
+          <Route path="crew" element={<Crew />} />
+          <Route path="technology" element={<Technology />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
