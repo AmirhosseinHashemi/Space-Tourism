4d6f146b99f4b86150b948d6f1e7eb58a100ead1
Amirhossein <4mirh03einhashemi@gmail.com>
2024-06-03 21:28:36 +0330

    set background image dynamicly

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import {Outlet} from "react-router-dom";
+import styled, {css} from "styled-components";
+import {Outlet, useHref} from "react-router-dom";
 
 import Header from "./Header";
 import Container from "./Container";
@@ -9,12 +9,38 @@ const StyledAppLayout = styled.div`
   min-height: 100dvh;
   background-repeat: no-repeat;
   background-position: top center;
-  background-image: url(images/home/background-home-mobile.jpg);
+
+  ${(prop) =>
+    prop["data-page"] === "home" &&
+    css`
+      background-image: url(images/home/background-home-mobile.jpg);
+    `}
+
+  ${(prop) =>
+    prop["data-page"] === "destination" &&
+    css`
+      background-image: url(images/destination/background-destination-mobile.jpg);
+    `}
+
+    ${(prop) =>
+    prop["aria-details"] === "crew" &&
+    css`
+      background-image: url(images/crew/background-crew-mobile.jpg);
+    `}
+
+    ${(prop) =>
+    prop["aria-details"] === "technology" &&
+    css`
+      background-image: url(images/technology/background-technology-mobile.jpg);
+    `}
 `;
 
 function AppLayout() {
+  const currentUrl = useHref();
+  const currentPage = currentUrl.slice(1).toLowerCase();
+
   return (
-    <StyledAppLayout>
+    <StyledAppLayout data-page={currentPage}>
       <Header />
       <Main>
         <Container>{<Outlet />}</Container>
