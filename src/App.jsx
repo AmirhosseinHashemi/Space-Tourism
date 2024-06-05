bbc87c5ed17276625134f1ef7c55a7abe32cd755
Amirhossein <4mirh03einhashemi@gmail.com>
2024-06-05 21:19:23 +0330

    complete routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import CrewProvider from "./contexts/CrewContext";
+
 import AppLayout from "./components/AppLayout";
+import Home from "./pages/Home";
+import Destination from "./pages/Destination";
+import Crew from "./pages/Crew";
+import Technology from "./pages/Technology";
+import TermProvider from "./contexts/TechContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate to="home" replace />} />
-          <Route path="home" element={<Home />} />
-          <Route path="destination" element={<Destination />} />
-          <Route path="crew" element={<Crew />} />
-          <Route path="technology" element={<Technology />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <CrewProvider>
+      <TermProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route index element={<Navigate to="home" replace />} />
+              <Route path="home" element={<Home />} />
+              <Route path="destination" element={<Destination />} />
+              <Route path="crew" element={<Crew />} />
+              <Route path="technology" element={<Technology />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </TermProvider>
+    </CrewProvider>
   );
 }
 
